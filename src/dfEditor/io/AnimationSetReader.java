b58dc8dfd063eb04003bd64fd4dbb58369a11126
Llewellyn Falco <isidore@setgame.com>
2015-11-05 14:38:44 -0500

    formatting

--- src/dfEditor/io/AnimationSetReader.java
+++ src/dfEditor/io/AnimationSetReader.java
@@ -16,21 +16,28 @@
  *  You should have received a copy of the GNU General Public License
  *  along with darkFunction Editor.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package dfEditor.io;
 
-import java.io.File;
-import javax.xml.parsers.*;
-import org.w3c.dom.*;
-import javax.swing.tree.DefaultTreeModel;
-import dfEditor.CustomNode;
-import dfEditor.SpriteTree;
-import java.awt.Rectangle;
-import java.util.ArrayList;
-import dfEditor.animation.*;
-import dfEditor.*;
-import java.awt.image.BufferedImage;
 import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import dfEditor.CustomNode;
+import dfEditor.GraphicObject;
+import dfEditor.SpriteGraphic;
+import dfEditor.SpriteTree;
+import dfEditor.animation.Animation;
+import dfEditor.animation.AnimationCell;
 
 /**
  *
@@ -40,221 +47,197 @@ import java.awt.Point;
  */
 public class AnimationSetReader
 {
-    private Document doc = null;
-    private File file = null;
-    private float version = 1;
-
-    public AnimationSetReader(File aFile) throws javax.xml.parsers.ParserConfigurationException, org.xml.sax.SAXException, java.io.IOException
+  private Document doc     = null;
+  private File     file    = null;
+  private float    version = 1;
+  public AnimationSetReader(File aFile) throws javax.xml.parsers.ParserConfigurationException,
+      org.xml.sax.SAXException, java.io.IOException
+  {
+    DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+    doc = builder.parse(aFile);
+    file = aFile;
+  }
+  public String getSpriteSheetPath()
+  {
+    NodeList setList = doc.getElementsByTagName("animations");
+    String spriteSheetName = ((Element) setList.item(0)).getAttribute("spriteSheet");
+    String versionString = ((Element) setList.item(0)).getAttribute("ver");
+    if (versionString != null)
     {
-        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-        doc = builder.parse(aFile);
-
-        file = aFile;
+      try
+      {
+        version = Float.parseFloat(versionString);
+      }
+      catch (java.lang.NumberFormatException nfe)
+      {
+        version = 1;
+      }
     }
-
-    public String getSpriteSheetPath()
+    String fullFileName = file.getParentFile().getPath() + "/" + spriteSheetName;
+    return fullFileName;
+  }
+  public ArrayList<Animation> getAnimations(SpriteTree aSpriteTree, BufferedImage aImage) throws Exception
+  {
+    ArrayList animations = new ArrayList<Animation>();
+    NodeList animNodeList = doc.getElementsByTagName("anim");
+    for (int i = 0; i < animNodeList.getLength(); ++i)
     {
-        NodeList setList = doc.getElementsByTagName("animations");
-        String spriteSheetName = ((Element)setList.item(0)).getAttribute("spriteSheet");
-        String versionString = ((Element)setList.item(0)).getAttribute("ver");
-        if (versionString != null)
+      Node animNode = animNodeList.item(i);
+      Animation animation = new Animation(((Element) animNode).getAttribute("name"));
+      String loopsStr = ((Element) animNode).getAttribute("loops");
+      if (loopsStr != null && loopsStr.length() != 0)
+      {
+        int loops = Integer.parseInt(loopsStr);
+        animation.setLoops(loops);
+      }
+      NodeList cellNodeList = animNode.getChildNodes();
+      for (int j = 0; j < cellNodeList.getLength(); ++j)
+      {
+        Node cellNode = cellNodeList.item(j);
+        if (cellNode.getNodeType() != Node.ELEMENT_NODE)
+          continue;
+        AnimationCell cell = new AnimationCell();
+        cell.setDelay(Integer.parseInt(((Element) cellNode).getAttribute("delay")));
+        NodeList spriteNodeList = cellNode.getChildNodes();
+        for (int k = 0; k < spriteNodeList.getLength(); ++k)
         {
-            try { 
-                version = Float.parseFloat(versionString);
-            } catch (java.lang.NumberFormatException nfe) {
-                version = 1;
-            }
-        }
-
-        String fullFileName = file.getParentFile().getPath() + "/" + spriteSheetName;
-
-        return fullFileName;
-    }
-
-    public ArrayList<Animation> getAnimations(SpriteTree aSpriteTree, BufferedImage aImage) throws Exception
-    {
-        ArrayList animations = new ArrayList<Animation>();
-        NodeList animNodeList = doc.getElementsByTagName("anim");
-
-        for (int i=0; i<animNodeList.getLength(); ++i)
-        {
-            Node animNode = animNodeList.item(i);
-            Animation animation = new Animation( ((Element)animNode).getAttribute("name") );
-
-            String loopsStr = ((Element)animNode).getAttribute("loops");
-            if (loopsStr != null && loopsStr.length() != 0)
+          Node spriteNode = spriteNodeList.item(k);
+          if (spriteNode.getNodeType() != Node.ELEMENT_NODE)
+            continue;
+          CustomNode treeNode = aSpriteTree.getNodeForPath(((Element) spriteNode).getAttribute("name"));
+          if (treeNode != null && treeNode.isLeaf())
+          {
+            GraphicObject spriteArea = (GraphicObject) treeNode.getCustomObject();
+            //                        boolean centreAnchor = false;
+            //                        String anchorString = ((Element)spriteNode).getAttribute("anchor");
+            //                        if (anchorString != null && anchorString.length() != 0)
+            //                        {
+            //                            if (anchorString.equals("centre"))
+            //                                centreAnchor = true;
+            //                        }
+            int x = 0;
+            int y = 0;
+            int z = 0;
+            try
             {
-                int loops = Integer.parseInt(loopsStr);
-                animation.setLoops(loops);
+              x = Integer.parseInt(((Element) spriteNode).getAttribute("x"));
+              y = Integer.parseInt(((Element) spriteNode).getAttribute("y"));
+              z = Integer.parseInt(((Element) spriteNode).getAttribute("z"));
             }
-
-            NodeList cellNodeList = animNode.getChildNodes();
-
-            for (int j=0; j<cellNodeList.getLength(); ++j)
+            catch (NumberFormatException e)
             {
-                Node cellNode = cellNodeList.item(j);
-                if (cellNode.getNodeType() != Node.ELEMENT_NODE)
-                    continue;
-
-                AnimationCell cell = new AnimationCell();
-                cell.setDelay( Integer.parseInt(((Element)cellNode).getAttribute("delay")) );
-
-                NodeList spriteNodeList = cellNode.getChildNodes();
-                for (int k=0; k<spriteNodeList.getLength(); ++k)
-                {
-                    Node spriteNode = spriteNodeList.item(k);
-                    if (spriteNode.getNodeType() != Node.ELEMENT_NODE)
-                        continue;
-                        
-                    CustomNode treeNode = aSpriteTree.getNodeForPath(
-                            ((Element)spriteNode).getAttribute("name")
-                            );
-
-                    if (treeNode != null && treeNode.isLeaf())
-                    {
-                        GraphicObject spriteArea = (GraphicObject)treeNode.getCustomObject();
-
-//                        boolean centreAnchor = false;
-//                        String anchorString = ((Element)spriteNode).getAttribute("anchor");
-//                        if (anchorString != null && anchorString.length() != 0)
-//                        {
-//                            if (anchorString.equals("centre"))
-//                                centreAnchor = true;
-//                        }
-                        
-                        int x=0;
-                        int y=0;
-                        int z=0;
-                        try {
-                            x = Integer.parseInt(((Element)spriteNode).getAttribute("x"));
-                            y = Integer.parseInt(((Element)spriteNode).getAttribute("y"));   
-                            z = Integer.parseInt(((Element)spriteNode).getAttribute("z"));   
-                        } catch (NumberFormatException e) {}
-                        
-                        
-                        Rectangle r = spriteArea.getRect();
-                                                
-                        if (version >= 1.2)
-                        {
-                            x -= r.width/2;
-                            y -= r.height/2;                            
-                        }
-                                               
-                        SpriteGraphic graphic = new SpriteGraphic(aImage, new Point(x, y), r);
-                        
-                        float angle = 0;
-                        String angleString = ((Element)spriteNode).getAttribute("angle");
-                        if (angleString != null && angleString.length() != 0)
-                            angle = Float.parseFloat(angleString);                        
-                        graphic.setAngle(angle);
-                        graphic.saveAngle();
-                        
-                        boolean flipH = false;
-                        boolean flipV = false;
-                        
-                        String flipHString = ((Element)spriteNode).getAttribute("flipH");
-                        String flipVString = ((Element)spriteNode).getAttribute("flipV");
-                        if (flipHString != null && flipHString.length() > 0)
-                            flipH = 0 != Integer.parseInt(flipHString);                                         
-                        if (flipVString != null && flipVString.length() > 0)
-                            flipV = 0 != Integer.parseInt(flipVString);                                         
-                        
-                        if (flipV)
-                            graphic.flip(false);
-                        if (flipH)
-                            graphic.flip(true);                            
-                        
-                        cell.addSprite(treeNode, graphic);
-                        cell.setZOrder(graphic, z);
-                    }
-                }
-
-                animation.addCell(cell);
             }
-            animations.add(animation);
+            Rectangle r = spriteArea.getRect();
+            if (version >= 1.2)
+            {
+              x -= r.width / 2;
+              y -= r.height / 2;
+            }
+            SpriteGraphic graphic = new SpriteGraphic(aImage, new Point(x, y), r);
+            float angle = 0;
+            String angleString = ((Element) spriteNode).getAttribute("angle");
+            if (angleString != null && angleString.length() != 0)
+              angle = Float.parseFloat(angleString);
+            graphic.setAngle(angle);
+            graphic.saveAngle();
+            boolean flipH = false;
+            boolean flipV = false;
+            String flipHString = ((Element) spriteNode).getAttribute("flipH");
+            String flipVString = ((Element) spriteNode).getAttribute("flipV");
+            if (flipHString != null && flipHString.length() > 0)
+              flipH = 0 != Integer.parseInt(flipHString);
+            if (flipVString != null && flipVString.length() > 0)
+              flipV = 0 != Integer.parseInt(flipVString);
+            if (flipV)
+              graphic.flip(false);
+            if (flipH)
+              graphic.flip(true);
+            cell.addSprite(treeNode, graphic);
+            cell.setZOrder(graphic, z);
+          }
         }
-        
-        return animations;
+        animation.addCell(cell);
+      }
+      animations.add(animation);
     }
-
-//    public ArrayList<Animation> getAnimations()
-//    {
-//        ArrayList<Animation> animationList = new ArrayList<Animation>();
-//
-//        NodeList animNodeList = doc.getElementsByTagName("anim");
-//        for (int i=0; i<animNodeList.getLength(); ++i)
-//        {
-//            Node animNode = animNodeList.item(i);
-//            Animation animation = new Animation( ((Element)animNode).getAttribute("name") );
-//
-//            NodeList cellNodeList = animNode.getChildNodes();
-//
-//            for (int j=0; j<cellNodeList.getLength(); ++j)
-//            {
-//                Node cellNode = cellNodeList.item(j);
-//                AnimationCell cell = new AnimationCell();
-//                cell.setDelay( Integer.parseInt(((Element)cellNode).getAttribute("delay")) );
-//                //cell.addSprite(null, null);
-//            }
-//
-//        }
-//
-//        return animationList;
-//    }
-
-
-//    public DefaultTreeModel getTreeModel()
-//    {
-//        DefaultTreeModel retTree = null;
-//
-//        NodeList defList = doc.getElementsByTagName("definitions");
-//
-//        // only support one bunch of definitions, use the first
-//        if (defList.getLength() > 0)
-//        {
-//            Element first = (Element)defList.item(0);
-//
-//            if (first != null && first.getFirstChild() != null)
-//            {
-//                retTree = createTreeModelFromDOM((Element)first.getFirstChild());
-//            }
-//        }
-//
-//        return retTree;
-//    }
-//
-//    private DefaultTreeModel createTreeModelFromDOM(Element aRootNode)
-//    {
-//        CustomNode rootNode = createCustomNodeFromDOMNode(aRootNode);
-//
-//        DefaultTreeModel treeModel = new DefaultTreeModel(rootNode, true);
-//
-//        return treeModel;
-//    }
-//
-//    public CustomNode createCustomNodeFromDOMNode(Element aDOMNode)
-//    {
-//        CustomNode node = new CustomNode(aDOMNode.getAttribute("name"), aDOMNode.hasChildNodes());
-//
-//        if (node.isLeaf() && aDOMNode.getTagName().equals("spr"))
-//        {
-//            node.setCustomObject(new SelectionBox(
-//                    new Rectangle(
-//                        Integer.parseInt(aDOMNode.getAttribute("x")),
-//                        Integer.parseInt(aDOMNode.getAttribute("y")),
-//                        Integer.parseInt(aDOMNode.getAttribute("w")),
-//                        Integer.parseInt(aDOMNode.getAttribute("h"))),
-//                    node.getColour()));
-//        }
-//        else
-//        {
-//            for (int i=0; i<aDOMNode.getChildNodes().getLength(); ++i)
-//            {
-//                CustomNode childNode = createCustomNodeFromDOMNode((Element)aDOMNode.getChildNodes().item(i));
-//                node.add(childNode);
-//            }
-//        }
-//
-//        return node;
-//    }
+    return animations;
+  }
+  //    public ArrayList<Animation> getAnimations()
+  //    {
+  //        ArrayList<Animation> animationList = new ArrayList<Animation>();
+  //
+  //        NodeList animNodeList = doc.getElementsByTagName("anim");
+  //        for (int i=0; i<animNodeList.getLength(); ++i)
+  //        {
+  //            Node animNode = animNodeList.item(i);
+  //            Animation animation = new Animation( ((Element)animNode).getAttribute("name") );
+  //
+  //            NodeList cellNodeList = animNode.getChildNodes();
+  //
+  //            for (int j=0; j<cellNodeList.getLength(); ++j)
+  //            {
+  //                Node cellNode = cellNodeList.item(j);
+  //                AnimationCell cell = new AnimationCell();
+  //                cell.setDelay( Integer.parseInt(((Element)cellNode).getAttribute("delay")) );
+  //                //cell.addSprite(null, null);
+  //            }
+  //
+  //        }
+  //
+  //        return animationList;
+  //    }
+  //    public DefaultTreeModel getTreeModel()
+  //    {
+  //        DefaultTreeModel retTree = null;
+  //
+  //        NodeList defList = doc.getElementsByTagName("definitions");
+  //
+  //        // only support one bunch of definitions, use the first
+  //        if (defList.getLength() > 0)
+  //        {
+  //            Element first = (Element)defList.item(0);
+  //
+  //            if (first != null && first.getFirstChild() != null)
+  //            {
+  //                retTree = createTreeModelFromDOM((Element)first.getFirstChild());
+  //            }
+  //        }
+  //
+  //        return retTree;
+  //    }
+  //
+  //    private DefaultTreeModel createTreeModelFromDOM(Element aRootNode)
+  //    {
+  //        CustomNode rootNode = createCustomNodeFromDOMNode(aRootNode);
+  //
+  //        DefaultTreeModel treeModel = new DefaultTreeModel(rootNode, true);
+  //
+  //        return treeModel;
+  //    }
+  //
+  //    public CustomNode createCustomNodeFromDOMNode(Element aDOMNode)
+  //    {
+  //        CustomNode node = new CustomNode(aDOMNode.getAttribute("name"), aDOMNode.hasChildNodes());
+  //
+  //        if (node.isLeaf() && aDOMNode.getTagName().equals("spr"))
+  //        {
+  //            node.setCustomObject(new SelectionBox(
+  //                    new Rectangle(
+  //                        Integer.parseInt(aDOMNode.getAttribute("x")),
+  //                        Integer.parseInt(aDOMNode.getAttribute("y")),
+  //                        Integer.parseInt(aDOMNode.getAttribute("w")),
+  //                        Integer.parseInt(aDOMNode.getAttribute("h"))),
+  //                    node.getColour()));
+  //        }
+  //        else
+  //        {
+  //            for (int i=0; i<aDOMNode.getChildNodes().getLength(); ++i)
+  //            {
+  //                CustomNode childNode = createCustomNodeFromDOMNode((Element)aDOMNode.getChildNodes().item(i));
+  //                node.add(childNode);
+  //            }
+  //        }
+  //
+  //        return node;
+  //    }
 }
